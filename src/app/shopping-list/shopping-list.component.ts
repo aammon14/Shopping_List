6acd5a308cdd8dcdd0510d3d5e177d65fb14d63e
andrewammon <andrewammon@usnyc022769.music.wmgis.com>
2018-09-21 10:55:53 -0400

    added toggle view to nav bar

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -10,7 +10,8 @@ import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
+    new Ingredient('Tomatoes', 10),
+    new Ingredient('Eggs', 12)
   ];
 
   constructor() { }
